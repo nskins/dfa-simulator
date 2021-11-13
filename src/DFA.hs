579d52ff86a6d113b0b1af4b1f61299a78d2df4d
Nicholas Skinsacos <nskins@umich.edu>
2021-11-13 12:46:19 -0500

    Implement the simulate function

--- src/DFA.hs
+++ src/DFA.hs
@@ -4,11 +4,14 @@ module DFA
   , simulate
   ) where
 
-import Data.HashSet (HashSet(..))
+import Data.Hashable (Hashable)
+import Data.HashSet (HashSet(..), member)
 
 type Delta a b = a -> b -> a
 
 data DFA a b = DFA (Delta a b) a (HashSet a)
 
-simulate :: DFA a b -> [b] -> Bool
-simulate (DFA delta initialState acceptStates) string = False
+simulate :: (Eq a, Hashable a) => DFA a b -> [b] -> Bool
+simulate (DFA delta initial accepts) string =
+  let finalState = foldl delta initial string
+  in member finalState accepts
